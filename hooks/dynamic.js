349b59219a0cddc411328f4bf41495c855cf26a9
Gregor <gregor@martynus.net>
2017-05-13 15:18:34 +0200

    fix: require session for sending email

--- hooks/dynamic.js
+++ hooks/dynamic.js
@@ -1,3 +1,7 @@
+var url = require('url')
+
+var sendRequest = require('request').defaults({json: true})
+
 /*
   Hooks allow you to alter the behaviour of hoodie-server,
   Hoodie’s core backend module.
@@ -55,20 +59,97 @@ module.exports = function (hoodie) {
       // main data store
 
       if (!request.payload) {
+        console.log('Email error: no body found ("%s", %s by %s)', email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
         return reply('no body found');
       }
 
       var email = request.payload;
 
-      hoodie.sendEmail(email, function (error) {
+      // Authorization:Bearer dXNlci9nQG1pbnV0ZXMuaW86NTgzMzFEQjg6HvQnojpLQfS_MsYoqORL9XjWBy0
+      var sessionId = (request.headers.Authorization || request.headers.authorization || '').substr('Bearer '.length)
+
+      if (!sessionId) {
+        console.log('Email error: sending email failed: no SessionID ("%s", %s by %s)', email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+        return reply('sending email failed: unauthenticated')
+      }
+
+      var userId = email.headers['X-MC-Metadata'].user_id
+      var couchUrl = url.parse(process.env.COUCH_URL) // {protocol, host}
+
+      // 1. find user account by id
+      sendRequest({
+        method: 'get',
+        url: url.format({
+          protocol: couchUrl.protocol,
+          host: couchUrl.host,
+          pathname: '/_users/_design/views/_view/fastspring-by-id',
+          search: '?key="' + userId + '"',
+          auth: [process.env.HOODIE_ADMIN_USER, process.env.HOODIE_ADMIN_PASS].join(':')
+        })
+      }, function (error, response, body) {
         if (error) {
-          return reply('sending email failed: "' + error + '"');
+          console.log('Email error: %s ("%s", %s by %s)', error.message, email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+          return reply(error)
         }
 
-        reply({'ok': true});
-      });
+        // expected body:
+        // {
+        //     "total_rows": 8,
+        //     "offset": 0,
+        //     "rows": [
+        //         {
+        //             "id": "org.couchdb.user:user/casper+1434488170262@minutes.io",
+        //             "key": "2w72y1c",
+        //             "value": null
+        //         }
+        //     ]
+        // }
 
-      return true;
+        if (!body.rows) {
+          console.log('Email error: sending email failed: account not found, case 1 ("%s", %s by %s)', email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+          return reply('sending email failed: unauthenticated')
+        }
+
+        if (body.rows.length === 0) {
+          console.log('Email error: sending email failed: account not found, case 2 ("%s", %s by %s)', email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+          return reply('sending email failed: unauthenticated')
+        }
+
+        var usersDocId = body.rows[0].id.split(':').pop()
+
+        sendRequest({
+          method: 'get',
+          url: url.format({
+            protocol: couchUrl.protocol,
+            host: couchUrl.host,
+            pathname: '/_users/org.couchdb.user:' + encodeURIComponent(usersDocId),
+          }),
+          headers: {
+            cookie: 'AuthSession=' + sessionId
+          }
+        }, function (error, response, body) {
+          if (error) {
+            console.log('Email error: Could not find user: %s ("%s", %s by %s)', error.message, email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+            return reply(error)
+          }
+
+          if (response.statusCode !== 200) {
+            console.log('Email error: Could not find user: %j ("%s", %s by %s)', response.body, email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+            console.log('Email error: sending email failed: account not found ("%s", %s by %s)', email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+            return reply('sending email failed: unauthenticated')
+          }
+
+          hoodie.sendEmail(email, function (error) {
+            if (error) {
+              console.log('Email error: sending email failed: %s ("%s", %s by %s)', error.message, email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+              return reply('sending email failed: "' + error + '"');
+            }
+
+            console.log('Email for "%s" sent (Meeting %s by %s)', email.subject, email.headers['X-MC-Metadata'].meeting_id, email.headers['X-MC-Metadata'].user_id)
+            reply({'ok': true});
+          });
+        })
+      })
     }
   };
 };
